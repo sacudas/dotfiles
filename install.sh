6a24621a921978454b3ecbc5c0f1ce9d7f0087a4
Barry Deeney <mxaddict@codedmaster.com>
2014-08-27 23:27:50 +0800

    Created the basic install script for my ~/dotfiles

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+# This is a very basic install script
+# that i use personally, but will soon
+# be updated to be useable by mostly anyone
+
+cp -r .bash_profile            ~/
+cp -r .bashrc                  ~/
+cp -r .bin/                    ~/
+cp -r .config/                 ~/
+cp -r .ctags                   ~/
+cp -r .fonts/                  ~/
+cp -r .gitconfig               ~/
+cp -r .promptline.sh           ~/
+cp -r .ssh/                    ~/
+cp -r .tmux.conf               ~/
+cp -r .vim/                    ~/
+cp -r .vimrc                   ~/
