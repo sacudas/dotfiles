08ff99d58cddf415984d6cb889809ada06ec4c2d
Barry Deeney <mxaddict@codedmaster.com>
2014-08-29 13:57:55 +0800

    Updated the README and install script to work with auto VIM Plugin install/setup

--- README.md
+++ README.md
@@ -9,5 +9,7 @@ Because I noticed that copying my Custom VIM, GIT, BASH and TMUX configs to work
 Installation
 ---
 ```shell
+# NOTE: make sure to install cmake, build-essentials and python-dev on ubuntu/debian
+#       for other distros, please install appropriate packages
 sh install.sh # seriously, that's it!
 ```
--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@
 # that i use personally, but will soon
 # be updated to be useable by mostly anyone
 
+# Initilize the git submodules
+# and update them!
+git submodule init
+git submodule update
+
+# Copy all the files needed
 cp -r .bash_profile            ~/
 cp -r .bashrc                  ~/
 cp -r .bin/                    ~/
@@ -17,3 +23,9 @@ cp -r .ssh/                    ~/
 cp -r .tmux.conf               ~/
 cp -r .vim/                    ~/
 cp -r .vimrc                   ~/
+
+# Install Vim Plugins
+vim +PluginInstall +qall
+
+cd ~/.vim/bundle/YouCompleteMe/
+./install.sh --clang-completer
