43de5bc30399622c8c2a0416630f97595880de84
Barry Deeney <mxaddict@codedmaster.com>
2014-09-04 15:01:31 +0800

    Updated the installer

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 # This is a very basic install script
 # that i use personally, but will soon
 # be updated to be useable by mostly anyone
@@ -11,21 +10,22 @@ git submodule init
 git submodule update
 
 # Copy all the files needed
-cp -r .bash_profile            ~/
-cp -r .bashrc                  ~/
-cp -r .bin/                    ~/
-cp -r .config/                 ~/
-cp -r .ctags                   ~/
-cp -r .fonts/                  ~/
-cp -r .gitconfig               ~/
-cp -r .promptline.sh           ~/
-cp -r .ssh/                    ~/
-cp -r .tmux.conf               ~/
-cp -r .vim/                    ~/
-cp -r .vimrc                   ~/
+rm -rf ~/.bash_profile      && cp -r .bash_profile      ~/
+rm -rf ~/.bashrc            && cp -r .bashrc            ~/
+rm -rf ~/.bin/              && cp -r .bin/              ~/
+rm -rf ~/.config/           && cp -r .config/           ~/
+rm -rf ~/.ctags             && cp -r .ctags             ~/
+rm -rf ~/.fonts/            && cp -r .fonts/            ~/
+rm -rf ~/.gitconfig         && cp -r .gitconfig         ~/
+rm -rf ~/.promptline.sh     && cp -r .promptline.sh     ~/
+rm -rf ~/.ssh/              && cp -r .ssh/              ~/
+rm -rf ~/.tmux.conf         && cp -r .tmux.conf         ~/
+rm -rf ~/.vim/              && cp -r .vim/              ~/
+rm -rf ~/.vimrc             && cp -r .vimrc             ~/
 
 # Install Vim Plugins
-vim +PluginInstall +qall
+vim +BundleClean +qall
+vim +BundleUpdate +qall
 
-cd ~/.vim/bundle/YouCompleteMe/
-./install.sh --clang-completer
+# Go to the YouCompleteMe DIR and compile and install it!
+~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
