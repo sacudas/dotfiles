6f633d4f5bf60efea280b24a7a4653d3319c3fd9
Barry Deeney <mxaddict@codedmaster.com>
2014-09-04 15:12:17 +0800

    Updated the installer to not delete the old files before install

--- install.sh
+++ install.sh
@@ -10,18 +10,18 @@ git submodule init
 git submodule update
 
 # Copy all the files needed
-rm -rf ~/.bash_profile      && cp -r .bash_profile      ~/
-rm -rf ~/.bashrc            && cp -r .bashrc            ~/
-rm -rf ~/.bin/              && cp -r .bin/              ~/
-rm -rf ~/.config/           && cp -r .config/           ~/
-rm -rf ~/.ctags             && cp -r .ctags             ~/
-rm -rf ~/.fonts/            && cp -r .fonts/            ~/
-rm -rf ~/.gitconfig         && cp -r .gitconfig         ~/
-rm -rf ~/.promptline.sh     && cp -r .promptline.sh     ~/
-rm -rf ~/.ssh/              && cp -r .ssh/              ~/
-rm -rf ~/.tmux.conf         && cp -r .tmux.conf         ~/
-rm -rf ~/.vim/              && cp -r .vim/              ~/
-rm -rf ~/.vimrc             && cp -r .vimrc             ~/
+cp -r .bash_profile      ~/
+cp -r .bashrc            ~/
+cp -r .bin/              ~/
+cp -r .config/           ~/
+cp -r .ctags             ~/
+cp -r .fonts/            ~/
+cp -r .gitconfig         ~/
+cp -r .promptline.sh     ~/
+cp -r .ssh/              ~/
+cp -r .tmux.conf         ~/
+cp -r .vim/              ~/
+cp -r .vimrc             ~/
 
 # Install Vim Plugins
 vim +BundleClean +qall
