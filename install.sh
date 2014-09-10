ad9575278d966952f539d4a248872f7f823c5eb4
Barry Deeney <mxaddict@codedmaster.com>
2014-09-10 12:38:32 +0800

    Updated the installer ot use PrintF instead of Echo as it works better on more systems!

--- install.sh
+++ install.sh
@@ -6,12 +6,13 @@
 
 # Initilize the git submodules
 # and update them!
-echo "Initilize repo submodules!"
+printf "Initilize repo submodules!\n"
 git submodule init > /dev/null 2>&1
 git submodule update > /dev/null 2>&1
 
 # Copy all the files needed
-echo "Copy all the files needed" cp -r .bash_profile      ~/
+printf "Copy all the files needed\n"
+cp -r .bash_profile      ~/
 cp -r .bashrc            ~/
 cp -r .bin/              ~/
 cp -r .config/           ~/
@@ -24,38 +25,38 @@ cp -r .tmux.conf         ~/
 cp -r .vim/              ~/
 
 # Ask what name to use?
-echo "Git Name [Default: $(git config --global user.name)]:"
+printf "Git Name [Default: $(git config --global user.name)]:"
 read GIT_NAME
 
 # Ask what email to use?
-echo "Git Email [Default: $(git config --global user.email)]:"
+printf "Git Email [Default: $(git config --global user.email)]:"
 read GIT_EMAIL
 
 # Save the GIT_NAME
 if [ "$GIT_NAME" != "" ]; then
-	echo "Save the GIT_NAME: $GIT_NAME"
+	printf "Save the GIT_NAME: $GIT_NAME\n"
 	git config --global user.name "$GIT_NAME"
 fi
 
 # Save the GIT_EMAIL
 if [ "$GIT_EMAIL" != "" ]; then
-echo "Save the GIT_EMAIL: $GIT_EMAIL"
+printf "Save the GIT_EMAIL: $GIT_EMAIL\n"
 	git config --global user.email "$GIT_EMAIL"
 fi
 
 # Install Vim Plugins
-echo "Install Vim Plugins, Will take a while depending on connection speed!"
+printf "Install Vim Plugins, Will take a while depending on connection speed!\n"
 vim +BundleClean +qall > /dev/null 2>&1
 vim +BundleUpdate +qall > /dev/null 2>&1
 
 # Copy the VIMRC file after bundle install so that we don't get errors!
-echo "Copy the VIMRC file after bundle install so that we don't get errors!"
+printf "Copy the VIMRC file after bundle install so that we don't get errors!\n"
 cp -r .vimrc             ~/
 
 # Go to the YouCompleteMe DIR and compile and install it!
-echo "Go to the YouCompleteMe DIR and compile and install it!"
-echo "This will also take a while, depending on CPU speed AND connection speed!"
+printf "Go to the YouCompleteMe DIR and compile and install it!\n"
+printf "This will also take a while, depending on CPU speed AND connection speed!\n"
 ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer > /dev/null 2>&1
 
 # Install done! WEW!
-echo "Install DONE! WEW!"
+printf "Install DONE! WEW!\n"
